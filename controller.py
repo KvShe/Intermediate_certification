3d1ad9df7f633912f4ab044091aa45581dd4b34d
Evgeniy <zheshe@list.ru>
2023-02-11 16:27:52 +0300

    add main

--- controller.py
+++ controller.py
@@ -2,24 +2,26 @@ import gui
 from gui import action_request, input_notes, find_by_date, find_notes, edit_note, del_not
 from work_whith_json import add, read, write
 
-while True:
-    match action_request():
-        case '1':
-            add(input_notes())
-        case '2':
-            gui.print_all(read())
-        case '3':
-            find_by_date(read())
-        case '4':
-            find_notes(read())
-        case '5':
-            dic = edit_note(read())
-            write(dic)
-        case '6':
-            dic = del_not(read())
-            write(dic)
-        case '7':
-            print('Приложение завершает работу')
-            break
-        case _:
-            print('Введённая команда не существует')
+
+def main():
+    while True:
+        match action_request():
+            case '1':
+                add(input_notes())
+            case '2':
+                gui.print_all(read())
+            case '3':
+                find_by_date(read())
+            case '4':
+                find_notes(read())
+            case '5':
+                dic = edit_note(read())
+                write(dic)
+            case '6':
+                dic = del_not(read())
+                write(dic)
+            case '7':
+                print('Приложение завершает работу')
+                break
+            case _:
+                print('Введённая команда не существует')
